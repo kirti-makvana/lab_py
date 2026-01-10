902b7ecc08e241c6536f3ecc490336982413ddb6
kirti-makvana <kirtimakvana1201@gmail.com>
2026-01-10 09:02:27 +0530

    assingment1

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello,world!" )
+hello,world!
+print("welcome to pythone programming")
+welcome to pythone programming
+name="kirti"
+age="19"
+print("Name:",name,"age:",age)
+Name: kirti age: 19
+name="kirti"
+print(f"Hello;{name}!welcome to python")
+Hello;kirti!welcome to python
