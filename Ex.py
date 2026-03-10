c11a8e8a48bf49626169f9f7d65b0cd15b01a950
kirti-makvana <kirtimakvana1201@gmail.com>
2026-03-10 09:21:01 +0530

    Add files via upload

---
+++ Ex.py
@@ -0,0 +1,13 @@
+#1. print number from 1 to 5.
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+#2.sum of numbers take user input.    
+num=int(input("Enter number:"))
+i=1
+d=0
+while i<=num:
+    d=d+i
+    i=i+1
+    print("sum=",d) 
