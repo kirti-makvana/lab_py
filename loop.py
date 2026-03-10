a3e3880cabc38606bc0893ea805cbd294a063f32
kirti-makvana <kirtimakvana1201@gmail.com>
2026-03-10 09:19:43 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,16 @@
+#looping struchar
+age=int(input("Enter your age:"))
+if age >= 18:
+    print("Yes, you are eligible for vote")    
+else:
+    print("No,you are not eligible for vote")    
+#1.
+marks=int(input("Enter your marks:"))
+if 0<30:
+    print("Your ,are faile")
+elif 34<=50:
+    print("C, grade")
+elif 50<=80:
+    print("B,grade")
+else:
+    print("A,grade congatulation!")
