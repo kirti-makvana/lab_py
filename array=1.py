ec7ef9fc16ca22918af31d722f559468746cf839
kirti-makvana <kirtimakvana1201@gmail.com>
2026-03-10 08:52:57 +0530

    Add files via upload

---
+++ array=1.py
@@ -0,0 +1,49 @@
+#1. importing array module
+from array import array
+arr=array('i',[10,20,30,40])
+print(arr)
+print(type(arr))
+#2. positive indexing
+from array import Array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])
+#3. negative indexing
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
+#4. modifying indexing
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4])
+print(arr[:3])
+print(arr[2:1])
+print(arr[:1])
+# with step
+from array import array
+arr=(array[10,20,30,40,50,60,70,80])
+print(arr[::2])
+print(arr[1::3])
+print(arr[:3])
+#negative
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-4:-1])
+print(arr[-3:])
+print(arr[:-2])
+#slicing
+from array import array
+arr=(array['i',10,20,30,40,50,60,70,80])
+print(arr[::2])
+print(arr[1::3])
+print(arr[::3])
+#reserve
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[::-1])
+#modifying
+from array import arrayarr=array('i',[10,20,30,40,50])
+arr[1:4]=array('i',[25,35,45])
+print(arr)
